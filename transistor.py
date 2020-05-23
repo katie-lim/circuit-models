89dfb8100d9c43b56c2e8d48127604b5559281bb
katie-lim <35044591+katie-lim@users.noreply.github.com>
2020-05-23 12:05:55 +0100

    Update transistor.py
    update code

--- transistor.py
+++ transistor.py
@@ -1,44 +1,64 @@
 import scipy as sp
 import matplotlib.pyplot as plt
-import math
 from scipy.integrate import quad
 
 # constants
-k_B = 1.38e-23
-q = 1.6021e-19
+kB = 1.38e-23
+e = 1.6021e-19
 
 R = 1
 C = 1
 
 def voltage(t):
-    if t < 50:
-        return 0
-    if t < 60:
-        return 10
-    else:
-        return 0
+    return 0.5*(sp.sign(t/10 - 5) + sp.sign(1 - (t/10 - 5))) # a rectangle function
+    # if t < 40:
+    #     return 0
+    # elif t < 50:
+    #     return 1
+    # else:
+    #     return 0
 
 def integrand(t):
-    return voltage(t) * math.exp(2*t/(R*C))
+    return voltage(t) * sp.exp(2*t/(R*C))
 
 #We need to firstly solve the top section of the circuit. All that needs to be done is computing the RHS integral.
-def V_1(t):
-    return voltage(t) - math.exp(-2*t/(R*C))/(R*C) * quad(integrand,0,t)[0]
 
-#Plotting V_1
-t = sp.arange(0, 101, 1)
-V = [V_1(i) for i in range(0,101)]
-plt.plot(t, V)
-plt.show()
+def q(t):
+    return sp.exp(-2*t/(R*C))/R * quad(integrand,0,t)[0]
 
-#Now we must consider the transistor
-Js=1
-T=300
+q = sp.vectorize(q) # Allow q(t) to take arrays as input
+
+def V1(t):
+    return voltage(t) - q(t)
 
 def transJ(t):
-    return Js*(math.exp(q*V_1(t)/(k_B*T))-math.exp(q*(V_1(t)-voltage(t))/(k_B*T)))
+    return Js * (sp.exp(e*V1(t)/(kB*T)) - sp.exp(e*(V1(t)-voltage(t))/(kB*T)))
 
-#Plotting transJ
-J = [transJ(i) for i in range(0,101)]
-plt.plot(t, J)
-plt.show()
+#Solve the ODE
+t = sp.linspace(0, 100, 200)
+q_values = q(t)
+
+#Now we must consider the transistor
+Js = 1
+T = 300
+J_values = transJ(t)
+
+#Finally, plot the results
+def plotResults():
+    fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(6, 8))
+
+    ax1.plot(t, voltage(t))
+    ax1.set_title("applied voltage vs time")
+
+    ax2.plot(t, q_values)
+    ax2.set_title("charge vs time")
+
+    ax3.plot(t, J_values)
+    ax3.set_title("current vs time")
+
+    plt.tight_layout()
+
+    plt.show()
+    return
+
+plotResults()
